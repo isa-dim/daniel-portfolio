df543beac322cc32c6efa0646d9751c9b7c8a0ff
isa-dim <idimarco92@gmail.com>
2022-02-07 22:21:02 +0100

    Create About.vue

---
+++ daniel-portfolio/src/pages/About.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+div.q-ma-lg.q-pt-lg
+  h5 I'm Daniel Cortés, a Germany based graphic designer and developer working on projects in fields ranging from identity, print, editorial design and art direction to websites and interactive design for businesses, individuals and cultural institutions. Please contact me if you’d like to discuss a project.
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
