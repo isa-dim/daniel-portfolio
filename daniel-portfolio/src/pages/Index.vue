25abfb1875a6fa070c8a618fa8ec0ae9b4bc321a
isa-dim <idimarco92@gmail.com>
2022-03-12 18:10:12 +0100

    test q-page-sticky, not sure

--- daniel-portfolio/src/pages/Index.vue
+++ daniel-portfolio/src/pages/Index.vue
@@ -1,10 +1,17 @@
 <template lang="pug">
-div.flex.test.window-height(:class="[$q.screen.lt.md ? 'column' : 'row']")
-  div.left-item
-    NaviMain
-    h3.titulo.q-pa-lg Film student with a love for images & stories
-  div.right-item.window-height
-    TableElement
+q-layout
+  div.flex.test.window-height(:class="[$q.screen.lt.md ? 'column' : 'row']")
+    div.left-item.full
+      NaviMain
+      h3.titulo.q-pa-lg(ref="text" @scroll="onScroll") Film student with a love for images & stories
+    q-page-sticky(position="right").right-item
+      div.window-height.width
+        TableElement(
+          v-for="project in projects"
+          :title="project.title"
+          :category="project.category"
+          :year="project.year"
+          )
 </template>
 
 <script>
@@ -14,22 +21,50 @@ import TableElement from 'components/TableElement'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { NaviMain, TableElement }
+  components: { NaviMain, TableElement },
+  methods: {
+    onScroll () {
+      console.log('hey')
+    }
+  },
+  data () {
+    return {
+      projects: [
+        {
+          title: 'Spain',
+          category: 'Photography Series',
+          year: '2021'
+        },
+        {
+          title: 'Portraits',
+          category: 'Photography Series',
+          year: '2020'
+        },
+        {
+          title: 'Aquí',
+          category: 'Short Film',
+          year: '2019'
+        }
+      ]
+    }
+  }
 })
 </script>
 
 <style  scoped lang="sass">
 .right-item
+  top: 0
   background: white
   color: $dc-green
-  flex: 1
-
+  // flex: 1
+  width: 50%
 .left-item
-  flex: 1
-
-// .right-item
-//     body.screen--xs &
-//       visibility: hidden
+  width: 50%
+  // flex: 1
+.width
+  width: 50vw
+.full
+  height: 4000px
 .titulo
   max-width: 9.5em
 </style>
