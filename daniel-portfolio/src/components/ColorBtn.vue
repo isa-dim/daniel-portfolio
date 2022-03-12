84d5ab2e8be92b51284ff09c0d2904cd5847b2cd
isa-dim <idimarco92@gmail.com>
2022-03-12 19:30:36 +0100

    Update ColorBtn - still missing functionality and position

--- daniel-portfolio/src/components/ColorBtn.vue
+++ daniel-portfolio/src/components/ColorBtn.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-div.circle.q-mb-xs
-div.circle-color.q-mb-xs
-div.circle-color-2.q-mb-xs
+div.q-ml-lg.circle.q-mb-xs(@click="show = !show")
+transition
+  div(v-if="show")
+    div.q-ml-lg.circle-color.q-mb-xs
+    div.q-ml-lg.circle-color-2.q-mb-xs
 </template>
 <script>
 export default {
@@ -9,7 +11,12 @@ export default {
   props: ['color'],
   data () {
     return {
-
+      show: false
+    }
+  },
+  methods: {
+    changeColor () {
+      console.log('hey')
     }
   }
 }
@@ -20,6 +27,10 @@ export default {
     width: 20px
     height: 20px
     border: 1px solid white
+  .circle:hover
+    box-shadow: 0px 0px 5px 4px rgba(255,255,255,0.53)
+    border: none
+    cursor: pointer
   .circle-color
     border-radius: 50%
     width: 20px
@@ -30,4 +41,11 @@ export default {
     width: 20px
     height: 20px
     background: blue
+  .v-enter-active,
+  .v-leave-active
+    transition: opacity 0.5s ease
+
+  .v-enter-from,
+  .v-leave-to
+    opacity: 0
 </style>
