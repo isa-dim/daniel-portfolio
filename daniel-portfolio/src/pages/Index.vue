99c4bcdbbd4b101f5e7623ec417c4083ed2f0d8f
isa-dim <idimarco92@gmail.com>
2022-03-12 18:55:26 +0100

    testing sticky page

--- daniel-portfolio/src/pages/Index.vue
+++ daniel-portfolio/src/pages/Index.vue
@@ -1,27 +1,27 @@
 <template lang="pug">
-q-layout
-  div.flex.test.window-height(:class="[$q.screen.lt.md ? 'column' : 'row']")
-    div.left-item.full
-      NaviMain
-      h3.titulo.q-pa-lg(ref="text" @scroll="onScroll") Film student with a love for images & stories
-    q-page-sticky(position="right").right-item
-      div.window-height.width
-        TableElement(
-          v-for="project in projects"
-          :title="project.title"
-          :category="project.category"
-          :year="project.year"
-          )
+div.flex.test.window-height(:class="[$q.screen.lt.md ? 'column' : 'row']")
+  div.left-item.full
+    NaviMain
+    h3.titulo.q-pa-lg(ref="text" @scroll="onScroll") Film student with a love for images & stories
+    ColorBtn(@click="showColorBtn = false")
+  div.right-item.window-height
+    TableElement(
+      v-for="project in projects"
+      :title="project.title"
+      :category="project.category"
+      :year="project.year"
+      )
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import NaviMain from 'components/NaviMain'
 import TableElement from 'components/TableElement'
+import ColorBtn from 'components/ColorBtn'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { NaviMain, TableElement },
+  components: { NaviMain, TableElement, ColorBtn },
   methods: {
     onScroll () {
       console.log('hey')
@@ -56,13 +56,9 @@ export default defineComponent({
   top: 0
   background: white
   color: $dc-green
-  // flex: 1
-  width: 50%
+  flex: 1
 .left-item
-  width: 50%
-  // flex: 1
-.width
-  width: 50vw
+  flex: 1
 .full
   height: 4000px
 .titulo
