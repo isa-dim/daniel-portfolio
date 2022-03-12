830d2837c96dd274aa2b273b4c635cef49a6ae84
isa-dim <idimarco92@gmail.com>
2022-03-12 18:55:08 +0100

    add color btn component for background color change

---
+++ daniel-portfolio/src/components/ColorBtn.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div.circle.q-mb-xs
+div.circle-color.q-mb-xs
+div.circle-color-2.q-mb-xs
+</template>
+<script>
+export default {
+  name: 'ColorBtn',
+  props: ['color'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="sass">
+  .circle
+    border-radius: 50%
+    width: 20px
+    height: 20px
+    border: 1px solid white
+  .circle-color
+    border-radius: 50%
+    width: 20px
+    height: 20px
+    background: pink
+  .circle-color-2
+    border-radius: 50%
+    width: 20px
+    height: 20px
+    background: blue
+</style>
