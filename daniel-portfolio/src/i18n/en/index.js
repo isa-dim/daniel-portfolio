75f6e5a7117e5d5e9931b8f57b3afc95f3c93872
isa-dim <idimarco92@gmail.com>
2022-04-04 09:20:01 +0200

    add i18n voc

---
+++ daniel-portfolio/src/i18n/en/index.js
@@ -0,0 +1,9 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  intro: 'Hi. My name is Daniel Cortés – a Germany based Colombian film student with a love for documentary, images & stories of all kinds. Currently looking for an internship or a job opportunity.',
+  contact: 'Please contact me if you would like to discuss a project.',
+  portfolio: 'Portfolio',
+  bigText: 'Film student with a love for images & stories'
+}
---
+++ daniel-portfolio/src/i18n/es/index.js
@@ -0,0 +1,9 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  intro: 'Hola. Soy Daniel Cortés – estudiante de cine de Colombia viviendo en Alemania. Amo los documentales, las imágenes y las historias de todo tipo. Estoy buscando una pasantía o una oportunidad de trabajo.',
+  contact: 'Si le gustaría trabajar conmigo, contácteme.',
+  portfolio: 'Portafolio',
+  bigText: 'Estudiante de cine perdido entre imágenes e historias'
+}
